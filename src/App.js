e98d444adb9f9dbfbcfa5b879290057f55293ddd
Jessica <jesscinsfb@gmail.com>
2021-05-28 11:08:20 -0700

    clean app.js file to remove unused components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import Navbar from "./Components/Navbar"
-import Footer from "./Components/Footer"
-import {BrowserRouter, Switch, Route} from "react-router-dom"
-import './App.css';
-import Home from "./pages/Home"
-import Projects from "./pages/Projects"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import AnchorLink from "react-anchor-link-smooth-scroll"
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Navbar/>
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/projects" component={Projects} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/contact" component={Contact} />
-      </Switch>
-      <Footer/>
-    </BrowserRouter>
-
+      <BrowserRouter>
+        <Navbar />
+        <Home />
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
