9b3c1d19afe2d1ea2461362e9e565d2b7736ab0a
Jessica <jesscinsfb@gmail.com>
2021-05-28 11:30:13 -0700

    cleanup project helper file

--- src/ProjectHelper.js
+++ src/ProjectHelper.js
@@ -28,12 +28,12 @@ let cards1 = [
     path: "/",
     github: "https://github.com/JessicaChen-48/react-personal-site"
   },
-  {
-    src: siteImg,
-    text: "TaxLion: React single-page application for calculating taxes for stocks",
-    label: "REACT",
-    path: "/"
-  },
+  // {
+  //   src: siteImg,
+  //   text: "TaxLion: React single-page application for calculating taxes for stocks",
+  //   label: "REACT",
+  //   path: "/"
+  // },
   {
     src: pokeballImg,
     text: "Poke Trainer: React site integrating with the PokeAPI",
@@ -43,41 +43,5 @@ let cards1 = [
   }
 ]
 
-let reactCards = [
-  {
-    src: joblyImg,
-    text: "Jobly: React front-end and RESTful API built with Express",
-    label: "REACT & EXPRESS",
-    path: "http://jobly.jessicaazchen.com"
-  },  {
-    src: siteImg,
-    text: "Personal Website: mobile-first, fully responsive React website",
-    label: "REACT",
-    path: "/"
-  },  {
-    src: pokeballImg,
-    text: "Poke Trainer: React site integrating with the PokeAPI",
-    label: "REACT",
-    path: "/services"
-  }
-]
 
-let expressCards = [
-  {
-    src: joblyImg,
-    text: "Jobly: React front-end and RESTful API built with Express",
-    label: "REACT & EXPRESS",
-    path: "http://jobly.jessicaazchen.com"
-  },
-]
-
-let flaskCards = [
-  {
-    src: warblerImg,
-    text: "Warbler: Twitter clone that uses SQL-Alchemy for database querying",
-    label: "FLASK & JINJA",
-    path: "https://warblerwarblering.herokuapp.com/"
-  }
-]
-
-export {cards1, cards2, reactCards, expressCards, flaskCards}
+export {cards1, cards2}
